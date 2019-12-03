75d5232a8334da7a400dbf34ae56816e5b214316
Andy Nowacki <a.nowacki@leeds.ac.uk>
2019-12-03 10:40:31 +0000

    Move voigt_kappa_mu out of io.jl

--- src/basic_properties.jl
+++ src/basic_properties.jl
@@ -61,3 +61,31 @@ end
 Return the surface radius in km of the `SeisModel` `m`.
 """
 surface_radius(m::SeisModel) = m.a
+
+"""
+    voigt_kappa_mu(A, C, L, N, F) -> κ, μ
+
+Return the Voigt average bulk modulus `κ` and shear modulus `μ` given the Love parameters
+of a transversely isotropic medium.  If A, C, etc., contain density, then κ and μ will be
+true moduli, whilst if the Love parameters are provided as velocities, then these
+are density-normalised 'moduli' with dimensions of L²⋅T².
+"""
+voigt_kappa_mu(A, C, L, N, F) = (4A + C + 4F - 4N)/9, (A + C - 2F + 5N + 6L)/15
+
+"""
+    voigt_velocities(vpv, vsv, vph, vsh, η) -> vp, vs
+
+Return the Voigt average `vp` and `vs`, in the same units as the input, given four
+velocities and the radial anisotropy parameter `η`.
+"""
+function voigt_velocities(vpv, vsv, vph, vsh, η)
+    A = vph^2
+    C = vpv^2
+    L = vsv^2
+    N = vsh^2
+    F = η*(A - 2L)
+    κ, μ = voigt_kappa_mu(A, C, L, N, F) # In km²/s²
+    vp = sqrt(κ + 4/3*μ)
+    vs = sqrt(μ)
+    vp, vs
+end
