b57b1fe1c94edac1f3fee020e3ca28f07f432738
Andy Nowacki <a.nowacki@leeds.ac.uk>
2020-07-28 16:43:45 +0100

    Add ek137 model of Kennett (GJI, 2020)
    This is exported as the inbuilt model `EK137`.

--- src/Earth/Earth.jl
+++ src/Earth/Earth.jl
@@ -14,9 +14,10 @@ List of Earth model instances as `Symbol`s.
 
 If adding new inbuilt Earth models, append the name of the new model to this list
 """
-const ALL_MODELS = (:AK135, :IASP91, :PREM, :STW105)
+const ALL_MODELS = (:AK135, :EK137, :IASP91, :PREM, :STW105)
 
 include("ak135.jl")
+include("ek137.jl")
 include("iasp91.jl")
 include("prem.jl")
 include("stw105.jl")
