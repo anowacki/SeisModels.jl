e229b0bfc7b62577dec8a6326118f904031a02c8
Andy Nowacki <a.nowacki@leeds.ac.uk>
2024-02-12 12:00:28 +0000

    Add a warning to docstrings of SC_REM models
    The `SC_REM_*` models are from a paper which has now been
    retracted (https://doi.org/10.1093/gji/ggae004), so add a
    warning to the docstrings for these models, and a note that
    they may be removed in a breaking version change.

--- src/Earth/sc_rem.jl
+++ src/Earth/sc_rem.jl
@@ -21,6 +21,12 @@ let data_path = joinpath(@__DIR__, "..", "..", "data", "Earth", "SC_REM")
             """
             # `$($model_name)`
 
+
+            !!! warning "Retracted model"
+                The $($model_name) model has been retracted by the authors and
+                is only retained in SeisModels to retain compatibility.
+                Future major versions of SeisModels may remove this model.
+
             $($lower_upper) bound of the $($percent)% credible interval of the SC-REM model of
             Kemper et al. (2023).
 
@@ -34,6 +40,9 @@ let data_path = joinpath(@__DIR__, "..", "..", "data", "Earth", "SC_REM")
 
             ## References
 
+            - Retraction of: Self-consistent models of Earth's mantle and core from long-period seismic and tidal constraints,
+              Geophysical Journal International, Volume 236, Issue 3, March 2024, Page 1439,
+              https://doi.org/10.1093/gji/ggae004
             - Kemper, J., Khan, A., Helffrich, G., van Driel, M., Giardini, D., 2023.
               Self-consistent models of Earth’s mantle and core from long-period seismic and tidal constraints.
               Geophys J Int 235, 690–717.
